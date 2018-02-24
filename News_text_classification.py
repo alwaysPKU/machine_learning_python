13247b9c7f1a75fb7379b57111a64cd3e125a185
alwaysqi <zw_pku@163.com>
2018-02-24 21:42:25 +0800

    add tfidfvectorizer and stopwords with countvectorizer and tfidvectorizer

--- News_text_classification.py
+++ News_text_classification.py
@@ -3,6 +3,7 @@ from sklearn.datasets import fetch_20newsgroups
 from sklearn.model_selection import train_test_split
 # 从sklearn.feature_extraction.text导入基于文本特征向量转化模块
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 # 导入朴素贝叶斯模型
 from sklearn.naive_bayes import MultinomialNB
 # 报告
@@ -17,17 +18,65 @@ print(len(news.data))
 print('============================================================')
 print(news.data[0])
 print('============================================================')
+print(news.target[0])
+print('============================================================')
 print(news.data[1])
 print('============================================================')
+print(news.target[1])
+print('============================================================')
 x_train, x_test, y_train, y_test = train_test_split(news.data, news.target, test_size=0.25, random_state=33)
+# 1. 使用CountVectorizer 并且不去掉停用词的条件下，对文本进行量化，朴素贝叶斯分类性能
 vec = CountVectorizer()
-x_train = vec.fit_transform(x_train)
-x_test = vec.transform(x_test)
+X_train = vec.fit_transform(x_train)
+X_test = vec.transform(x_test)
 mnb = MultinomialNB()
-mnb.fit(x_train, y_train)
-y_predict = mnb.predict(x_test)
-
-print('the accuracy of naive Bayes classifier is:', mnb.score(x_test, y_test))
+mnb.fit(X_train, y_train)
+y_predict = mnb.predict(X_test)
+print('#########使用CountVectorizer 并且不去掉停用词的条件下，对文本进行量化，朴素贝叶斯分类性能############')
+print('the accuracy of naive Bayes classifier is:', mnb.score(X_test, y_test))
 print('============================================================')
 print(classification_report(y_test, y_predict, target_names=news.target_names))
 
+# 2. 使用TfidfVectorizer并且不去掉停用词的条件下，对文本向量化，朴素贝叶斯分类性能
+tfidf_vec = TfidfVectorizer()
+X_tfdif_train = tfidf_vec.fit_transform(x_train)
+X_tfdif_test = tfidf_vec.transform(x_test)
+mnb_tifdif = MultinomialNB()
+mnb_tifdif.fit(X_tfdif_train, y_train)
+y_tifdif_predict = mnb_tifdif.predict(X_tfdif_test)
+print('#########使用TfidfVectorizer并且不去掉停用词的条件下，对文本向量化，朴素贝叶斯分类性能############')
+print('the accuracy of naive Bayes classifier is:', mnb.score(X_tfdif_test, y_test))
+print('============================================================')
+print(classification_report(y_test, y_tifdif_predict, target_names=news.target_names))
+
+# 3. 使用CountVectorizer与 并且去掉停用词的条件下，对文本进行量化，朴素贝叶斯分类性能
+
+print('#########使用CountVectorizer与TfidfVectorizer并且去掉停用词的条件下，对文本进行量化，朴素贝叶斯分类性能############')
+count_filter_vec, tifdif_filter_vec = \
+    CountVectorizer(analyzer='word', stop_words='english'), \
+    TfidfVectorizer(analyzer='word', stop_words='english')
+
+X_count_filter_train = count_filter_vec.fit_transform(x_train)
+X_count_filter_test = count_filter_vec.transform(x_test)
+
+X_tifdif_filter_train = tifdif_filter_vec.fit_transform(x_train)
+X_tifdif_filter_test = tifdif_filter_vec.transform(x_test)
+
+mnb_count_filter = MultinomialNB()
+mnb_count_filter.fit(X_count_filter_train, y_train)
+y_count_filter_predict = mnb_count_filter.predict(X_count_filter_test)
+print('============================================================')
+print('the accuracy of classifying 20newsgroups using naive bayes(countVectorizer by filtering stopwords): ',
+      mnb_count_filter.score(X_count_filter_test, y_test))
+print('============================================================')
+print(classification_report(y_test, y_count_filter_predict, target_names=news.target_names))
+print('============================================================')
+
+mnb_tifdif_filter = MultinomialNB()
+mnb_tifdif_filter.fit(X_tifdif_filter_train, y_train)
+y_tifdif_filter_predict = mnb_tifdif_filter.predict(X_tifdif_filter_test)
+print('============================================================')
+print('the accuracy of classifying 20newsgroups using naive bayes(TfidfVectorizer by filtering stopwords:',
+      mnb_tifdif_filter.score(X_tifdif_filter_test, y_test))
+print('============================================================')
+print(classification_report(y_test, y_tifdif_filter_predict, target_names=news.target_names))
