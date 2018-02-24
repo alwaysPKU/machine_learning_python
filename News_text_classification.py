52d500b05c4a21bac1ce3ce7f942efc039dbeaf3
alwaysqi <zw_pku@163.com>
2018-02-24 20:51:25 +0800

    CountVectorizer

--- News_text_classification.py
+++ News_text_classification.py
@@ -12,9 +12,13 @@ from sklearn.metrics import classification_report
 # 与之前预存的数据不同，fetch_20newsgroups需要从互联网上即时下载
 news = fetch_20newsgroups(subset='all')
 # 检查数据规模和细节
+print('============================================================')
 print(len(news.data))
+print('============================================================')
 print(news.data[0])
+print('============================================================')
 print(news.data[1])
+print('============================================================')
 x_train, x_test, y_train, y_test = train_test_split(news.data, news.target, test_size=0.25, random_state=33)
 vec = CountVectorizer()
 x_train = vec.fit_transform(x_train)
@@ -24,5 +28,6 @@ mnb.fit(x_train, y_train)
 y_predict = mnb.predict(x_test)
 
 print('the accuracy of naive Bayes classifier is:', mnb.score(x_test, y_test))
+print('============================================================')
 print(classification_report(y_test, y_predict, target_names=news.target_names))
 
